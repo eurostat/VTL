2135ea734f292fb74a3923c931050facd15fe95d
Maurizio <maurizio.capaccioli@ec.europa.eu>
2018-07-12 18:19:07 +0200

    Add files via upload

---
+++ VTL.cmd
@@ -0,0 +1,6 @@
+rem C:\Windows\System32\cmd.exe /appvve:8f39732f-0992-41ea-a294-c029a4242de9_db32b125-5384-4471-a2bf-ae0334e81624 
+
+java -Xms128m -Xmx512m -jar VTL.jar olrdev2.cc.cec.eu.int:1597/EBASESTA_TAF.cc.cec.eu.int refread Tv.69A5U3=4
+
+pause
+
